62eae2d1ed551b545f8e777bf749d384b4b6ae63
Abondarenko29 <myobvicoviyzapis@gmail.com>
2025-02-08 12:10:13 +0200

    add_some_things

---
+++ booking/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Room(models.Model):
+    number = models.CharField(max_length=5)
+    capacity = models.PositiveIntegerField()
+    # image = models.ImageField()
+    location = models.CharField(max_length=3900)
+    description = models.TextField(blank=True, null=True)
+    area = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.number} at {self.capacity}"
+
+    class Meta:
+        verbose_name = "room"
+        verbose_name_plural = "rooms"
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="booking")
+    room = models.ForeignKey(Room, on_delete=models.SET("1"),
+                             related_name="room")
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} books {self.room}"
+
+    class Meta:
+        verbose_name = "booking"
+        verbose_name_plural = "bookings"
---
+++ booking/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import room_list
+
+
+urlpatterns = [
+    path("", room_list, name="room_list")
+]
